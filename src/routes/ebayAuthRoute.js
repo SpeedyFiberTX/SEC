e17f5ff4f8259b27bd3b011bef85b437135f3fa5
speedyfibertw@outlook.com <speedyfibertw@outlook.com>
2025-09-10 12:44:37 +0800

    fix:加上render環境變數並改為URLSearchParams組裝

--- src/routes/ebayAuthRoute.js
+++ src/routes/ebayAuthRoute.js
@@ -11,23 +11,10 @@ dotenv.config();
 // APP資訊
 const ebay_client_id = process.env.EBAY_CLIENT_ID;
 // const ebay_client_secret = process.env.EBAY_CLIENT_SECRET;
+const ebay_scopes = process.env.EBAY_SCOPES;
 const ebay_ru_name = process.env.EBAY_RU_NAME;
 
 const endpoint = 'https://auth.sandbox.ebay.com/oauth2/authorize';
-
-// 處理scope
-// 從 .env 取出
-const rawScopes = process.env.EBAY_SCOPES || "";
-
-// 以空白拆開，去掉空字串
-const scopes = rawScopes.split(/\s+/).filter(Boolean);
-
-// 正確組合成一個字串（中間用空白）
-const scopeString = scopes.join(" ");
-// scope問題結束
-
-// encode 成 eBay 需要的格式（空白 → %20）
-const encodedScope = encodeURIComponent(scopeString);
 
 const router = express.Router();
 
@@ -38,14 +25,15 @@ function generateState() {
 router.get('/login', async (req, res) => {
     const state = generateState();
 
-    const authUrl = `https://auth.sandbox.ebay.com/oauth2/authorize?` +
-        `client_id=${encodeURIComponent(ebay_client_id)}` +
-        `&redirect_uri=${encodeURIComponent(ebay_ru_name)}` +
-        `&response_type=code` +
-        `&scope=${encodedScope}` +
-        `&state=${encodeURIComponent(state)}`;
+    const params = new URLSearchParams({
+        client_id: ebay_client_id,
+        redirect_uri: ebay_ru_name,
+        response_type: 'code',
+        scope: ebay_scopes,
+        state: state,
+    });
 
-    console.log("[authUrl]", authUrl);
+    const authUrl = `${endpoint}?${params}`;
     res.redirect(authUrl);
 
 })
