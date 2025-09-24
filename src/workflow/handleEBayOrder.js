5b0699bfac4de2029997e714e650284c59ac68b3
speedyfibertw@outlook.com <speedyfibertw@outlook.com>
2025-09-24 18:00:47 +0800

    fix:改回1小時&傳送給群組

--- src/workflow/handleEBayOrder.js
+++ src/workflow/handleEBayOrder.js
@@ -431,8 +431,8 @@ export default async function handleEBayOrder() {
             }
 
             // 送出 Line 訊息
-            // await pushMessageToMe(`過去 1 小時 eBay 共有 ${orderList.length}筆新訂單進來囉：\n${lineMessage.join('\n---\n')}`)
-            await pushMessageToDeveloper(`過去 1 小時 eBay 共有 ${orderList.length}筆新訂單進來囉：\n${lineMessage.join('\n---\n')}`)
+            await pushMessageToMe(`過去 1 小時 eBay 共有 ${orderList.length}筆新訂單進來囉：\n${lineMessage.join('\n---\n')}`)
+            // await pushMessageToDeveloper(`過去 1 小時 eBay 共有 ${orderList.length}筆新訂單進來囉：\n${lineMessage.join('\n---\n')}`)
 
             // 送出到 notion
             console.log("📝 開始新增資料到平台訂單彙整...");
@@ -483,5 +483,3 @@ export default async function handleEBayOrder() {
         console.error("❌ eBay 訂單處理錯誤：", err?.message || err);
     }
 }
-
- handleEBayOrder()
